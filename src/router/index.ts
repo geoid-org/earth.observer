76278fba34de49826f4bf2b602612bae413f0af7
Lars van Vianen <lars@vianen.com>
2023-07-28 21:25:44 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,24 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/data",
+    name: "data",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/DataView.vue"),
+  },
+  {
+    path: "/resources",
+    name: "resources",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ResourcesView.vue"),
+  },
 ];
 
 const router = createRouter({
