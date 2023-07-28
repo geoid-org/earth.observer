8810780dcb84f58198b690de33c80d46b9744e1a
Lars van Vianen <lars@vianen.com>
2023-07-28 21:16:47 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     // parser: 'babel-eslint'
-
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
