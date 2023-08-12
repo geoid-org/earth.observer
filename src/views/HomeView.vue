95fb32cf455e3a70181ea96ab06acdfdd2d785e3
Lars van Vianen <lars@vianen.com>
2023-08-12 14:01:36 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img class="logo" alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Earth Observer" />
-  </div>
+    <div class="home">
+        <img class="logo" alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Earth Observer" />
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,15 +10,15 @@ import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld
+    }
 })
 export default class HomeView extends Vue {}
 </script>
 
 <style scoped>
 .logo {
-  width: 200px;
+    width: 200px;
 }
 </style>
