3807d461c8df68ea32e2501db5bd051c51b4f253
Lars van Vianen <lars@vianen.com>
2023-08-12 14:00:55 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    // browser: true,
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    // "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    // parser: 'babel-eslint'
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    rules: {
+        semi: "error",
+        "prefer-const": "error",
+        "prettier/prettier": "error",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    },
+    env: {
+        // browser: true,
+        node: true
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        // "eslint:recommended",
+        "@vue/typescript/recommended",
+        "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+        // parser: 'babel-eslint'
+    }
 };
