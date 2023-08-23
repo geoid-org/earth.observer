a254c0184fdc49e5bbaf9bca89d65bd913f1d763
Lars van Vianen <lars@vianen.com>
2023-08-23 10:03:01 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
         "prefer-const": "error",
         "prettier/prettier": "error",
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        // "@typescript-eslint/no-unused-vars-experimental": "error",
+        "no-unused-vars": "off"
     },
     env: {
         // browser: true,
@@ -14,6 +17,8 @@ module.exports = {
     extends: [
         "plugin:vue/vue3-essential",
         // "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
         "@vue/typescript/recommended",
         "plugin:prettier/recommended"
     ],
