da0424ca5a6a6decb79c2a9786e25b51eba1f80d
Lars van Vianen <lars@vianen.com>
2023-08-27 15:14:09 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+    chainWebpack(config) {
+        config.entry("app").clear().add("./src/main.ts").end();
+        config.resolve.alias
+            .set("~", path.join(__dirname, "./node_modules"))
+            .set("@", path.join(__dirname, "./src"))
+            .set("#", path.join(__dirname, "./src"));
+    },
+    transpileDependencies: true,
+    css: {
+        sourceMap: true,
+        loaderOptions: {
+            sass: {
+                additionalData: `
+                @import "@/assets/style/main.scss";
+                `
+            }
+        }
+    },
+    assetsDir: "@/assets/"
 });
