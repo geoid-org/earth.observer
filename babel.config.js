0ed366bb91af2f103a821709a668a4f78665315e
Lars van Vianen <lars@vianen.com>
2023-08-27 15:14:04 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
 };
+// module.exports = {
+//   presets: [
+//     [
+//       "@babel/preset-env",
+//       {
+//         useBuiltIns: "usage",
+//         corejs: 3,
+//       },
+//     ],
+//   ],
+// };
