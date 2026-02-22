c4ed77295e692544258856c2cdd0a1ad06bd37ba
Lars van Vianen <lars@vianen.com>
2026-02-22 21:37:03 +0100

    Fix Airtable lambda, format code and mockdata
    Refactor lambda/airtable.js for consistent styling and correctness (normalized quotes/semicolons, properly call fetchNextPage in eachPage and ensure callback responses). Compress repeated multi-line arrays in lambda/mockdata.json to single-line arrays to reduce noise and file size. Update .vscode/settings.json to use explicit ESLint fixAll setting and wrap a paragraph in the exported markdown for line-length/readability. Also include minor TypeScript/shim formatting tweaks in src/.

--- src/lambdaService.ts
+++ src/lambdaService.ts
@@ -1,29 +1,28 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
 
 const $axios: AxiosInstance = axios.create({
-  baseURL: '/.netlify/functions',
-  timeout: 10000, // 10 seconds
+    baseURL: "/.netlify/functions",
+    timeout: 10000 // 10 seconds
 });
 
 // Response Interceptor to handle and log errors
 $axios.interceptors.response.use(
     (response: AxiosResponse) => {
-      return response;
+        return response;
     },
     (error: AxiosError) => {
-      return Promise.reject(error);
+        return Promise.reject(error);
     }
 );
 
-
 interface ResponseData {
-  // specify the data type
+    // specify the data type
 }
 
 export default {
-  fetchRecords(): Promise<ResponseData> {
-    return $axios.get<ResponseData>('airtable').then(response => {
-      return response.data;
-    });
-  }
+    fetchRecords(): Promise<ResponseData> {
+        return $axios.get<ResponseData>("airtable").then((response) => {
+            return response.data;
+        });
+    }
 };
